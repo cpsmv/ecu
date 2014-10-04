ac3b93eb5390a0c22632bcaa450c38f82bf47a8d
Ivan Pachev <ipachev@gmail.com>
2014-10-04 16:48:19 -0700

    Moved stuff around

---
+++ Arduino/src/avr-pinterrupt.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define intPin 2
+#define ledPin 13
+
+volatile int state;
+
+void interruptlol(){
+    digitalWrite(ledPin, state=!state);
+}
+
+int main()
+{
+  state = LOW;
+  pinMode(intPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  
+  // initialize timer1 
+  noInterrupts();           // disable all interrupts
+  attachInterrupt(0,interruptlol, CHANGE);
+  interrupts();             // enable all interrupts
+  Serial.begin(115200);
+  
+  while(1);
+}
+
---
+++ Arduino/src/avr-timertest.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define ledPin 13
+
+int main()
+{
+  pinMode(ledPin, OUTPUT);
+  
+  // initialize timer1 
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;  // no config settings in this register
+  TCCR1B = 0;  // init to zero but waiting for config settings
+  TCNT1  = 0;  // init timer 1 counter to zero
+
+  OCR1A = 65535;            // compare match register 16MHz/256/2Hz
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= (1 << CS02) | (1 << CS00);    // 1024 prescaler 
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  interrupts();             // enable all interrupts
+  Serial.begin(115200);
+
+  while(1) {
+    Serial.println("test");
+  }
+}
+
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+{
+  digitalWrite(ledPin, !digitalRead(ledPin));
+  if(OCR1A > 1000) OCR1A /=2 ;   // toggle LED pin
+}
