efe381e6d12ced939e78030ade0c54b0190f984a
Ivan Pachev <ipachev@gmail.com>
2014-10-07 17:30:24 -0700

    Add back and forth switching timer example

--- Arduino/src/avr-timertest.cpp
+++ Arduino/src/avr-timertest.cpp
@@ -7,6 +7,9 @@ const unsigned int prescalers[] = {
   0, 1, 8, 64, 256, 1024};
   
 unsigned int q,i;
+char ledon;
+long oldtimer;
+long currtimer;
 
 void configTimer(long m) {
   noInterrupts();
@@ -21,6 +24,7 @@ void configTimer(long m) {
       break;
     }
   }
+  currtimer = m;
   interrupts();
 }
 
@@ -29,7 +33,9 @@ int main() {
   Serial.begin(115200);
   Serial.println("SUP");
   // initialize timer1
-  configTimer(1000000); // configure timer for 1 s
+  ledon=0;
+  configTimer(1000000);
+  oldtimer = 2000000;// configure timer for 1 s
   
   Serial.print("regster set to ");
   Serial.println(q);
@@ -40,7 +46,18 @@ int main() {
 }
 
 ISR(TIMER1_COMPA_vect) {
- digitalWrite(ledPin, digitalRead(ledPin) ^ 1); 
+  if(ledon) {
+    digitalWrite(ledPin, 0);
+    configTimer(oldtimer);
+    oldtimer = currtimer;
+    ledon = 1;
+  }
+  else {
+    digitalWrite(ledPin, 1);
+    configTimer(oldtimer);
+    oldtimer = currtimer;
+    ledon = 0;
+  }
 }
 
 
